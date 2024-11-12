e718c30f7abaf45108639777e98549e6e1d5bc5f
mvtalan <mvtalan@gmail.com>
2024-11-11 19:48:03 -0700

    complete crash course

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Posts from './routes/Posts'
-import NewPost from './routes/NewPost';
+import Posts, { loader as postsLoader } from './routes/Posts'
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails';
 import RootLayout from './routes/RootLayout';
 import './index.css'
 
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
     children: [
       { path: '/', 
         element: <Posts />, 
-        children: [{ path: '/create-post', element: <NewPost /> }] 
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader }
+        ] 
       },
   ] },
 ]);
